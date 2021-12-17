d03ca7cb4f5b52541759a50eae0986cb055cb96a
nikunjs21 <56684114+nikunjs21@users.noreply.github.com>
2021-12-18 00:06:02 +0530

    CORS error solve

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -1,4 +1,24 @@
 <?php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+	//If required
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Max-Age: 86400'); // cache for 1 day
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+		header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+	}
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	}
+
+	exit(0);
+}
+
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
